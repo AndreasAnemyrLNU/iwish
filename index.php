e9638d9e7c57365eecf727edd722f08ec805c590
AndréasAnemyrLNU <aa223ig@student.lnu.se>
2015-11-11 21:12:40 +0100

    ....

--- index.php
+++ index.php
@@ -11,18 +11,18 @@
 
     //module
     //github
-    require_once("model/github/added.php");
-    require_once("model/github/author.php");
-    require_once("model/github/commits.php");
-    require_once("model/github/committer.php");
+    require_once("model/github/Added.php");
+    require_once("model/github/Author.php");
+    require_once("model/github/Commits.php");
+    require_once("model/github/Committer.php");
     require_once("model/github/HeadCommit.php");
-    require_once("model/github/modified.php");
+    require_once("model/github/Modified.php");
     require_once("model/github/Owner.php");
     require_once("model/github/Pusher.php");
-    require_once("model/github/removed.php");
+    require_once("model/github/Removed.php");
     require_once("model/github/Repository.php");
     require_once("model/github/Sender.php");
-    require_once("model/github/webhook.php");
+    require_once("model/github/Webhook.php");
     require_once("view/GitPaylod.php");
     require_once("controller/GitController.php");
 
