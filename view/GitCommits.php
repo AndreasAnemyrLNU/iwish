61cf2558da60a9aed2a69a8cd1ae05ed5c5f33e4
AndréasAnemyrLNU <aa223ig@student.lnu.se>
2015-11-12 12:23:19 +0100

    continious integration :)

--- view/GitCommits.php
+++ view/GitCommits.php
@@ -21,13 +21,15 @@ class GitCommits
     public function getHTML()
     {
 
-        return "<div class = 'panel panel-primary'>
-                    <div class = 'panel-heading'>
-                        <h3 class = 'panel-title'>History of commits to github for this project!</h3>
-                    </div>
-                    <div class = 'panel-body'>
-                        <div class='container'>
-                            {$this->ExtractWebhook()}
+        return "<div class='col-xs-10 col-xs-offset-1 col-sm-10 col-sm-offset-1 col-md-10 col-md-offset-1 col-lg-10 col-lg-offset-1'>
+                    <div class = 'panel panel-primary'>
+                        <div class = 'panel-heading'>
+                            <h3 class = 'panel-title'>History of commits to github for this project!</h3>
+                        </div>
+                        <div class = 'panel-body'>
+                            <div class='container col-xs-10 col-xs-offset-1 col-sm-10 col-sm-offset-1 col-md-10 col-md-offset-1 col-lg-10 col-lg-offset-1'>
+                                {$this->ExtractWebhook()}
+                            </div>
                         </div>
                     </div>
                 </div>";
@@ -47,12 +49,12 @@ class GitCommits
     private function RenderWebHook(\model\Webhook $w)
     {
         return "<div class='row'>
-                    <div class='span2'>
+                    <div class='col-xs-10 col-sm-10 col-md-10 col-lg-10'>
                         <p>{$w->getAfter()}</p>
                         <p>{$w->getBefore()}</p>
                         <p>{$this->GetRepository($w->getRepository())->getId()}</p>
                     </div>
-                    <div class='span2'>
+                    <div class='col-xs-2 col-sm-2 col-md-2 col-lg-2'>
                         <img src={$this->GetSender($w->getSender())->getAvatarUrl()} class='img-responsive img-circle' alt='Avatar Pic'>
                     </div>
                 </div>";
--- view/IndexPage.php
+++ view/IndexPage.php
@@ -22,9 +22,11 @@ class indexPage
         return
         "
         <!DOCTYPE html>
-        <html lang=\"sv\">
+        <html lang='sv'>
             <head>
-            <meta charset=\"UTF-8\">
+            <meta charset='UTF-8'>
+            <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+            <meta name='viewport' content='width=device-width, initial-scale=1'>
             $bootstrapCSSHTML
             <title></title>
             </head>
