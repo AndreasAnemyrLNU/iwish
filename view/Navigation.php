736d1470fcb7c800e4bb8edd494e551d8b48c3d5
AndréasAnemyrLNU <aa223ig@student.lnu.se>
2015-11-14 14:39:37 +0100

    continious integration :)

--- view/Navigation.php
+++ view/Navigation.php
@@ -11,5 +11,27 @@ namespace view;
 
 class Navigation
 {
+    //**********************************************************************************
+    // Key Readonly
+    private static $controllerKey = 'c';
+    //**********************************************************************************
+    public static function getControllerKey(){return self::$controllerKey;}
+    // Value for private $controller to use.
+    // I You declare a new controller you should declare it here!
+    //**********************************************************************************
+    private static $indexControllerValue = 'index';
+    public static function getIndexControllerValue(){return self::$indexControllerValue;}
+    //**********************************************************************************
+    private static $gitControllerValue = 'git';
+    public static function getGitControllerValue(){return self::$gitControllerValue;}
+    //**********************************************************************************
+    private static $downLoadControllerValue = 'download';
+    public static function getDownLoadControllerValue(){return self::$downLoadControllerValue;}
 
+    // Start Region :: Evaluate controller
+    public function ClientWantsTheDownloadController()
+    {
+        if($_POST[self::$controllerKey] === self::$indexControllerValue)
+            return true;
+    }
 }
